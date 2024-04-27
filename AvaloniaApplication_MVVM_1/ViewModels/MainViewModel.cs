ebecaa810783abb9f1cd380b3db59078c78a66a2
AlexD972 <alexandreduclos83@gmail.com>
2024-04-27 22:40:58 +0200

    Ajoutez des fichiers projet.

---
+++ AvaloniaApplication_MVVM_1/ViewModels/MainViewModel.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.ObjectModel;
+using System.Xml.Linq;
+
+namespace AvaloniaApplication_MVVM_1.ViewModels;
+
+public partial class MainViewModel : ViewModelBase
+{
+	public ObservableCollection<MyData> YourCollection { get; set; }
+
+	public MainViewModel()
+	{
+		YourCollection = new ObservableCollection<MyData>();
+
+		for (int i = 0; i < 21; i++)
+		{
+			YourCollection.Add(new MyData
+			{
+				Propriete1 = "N/A",
+				Propriete0 = " " + (10*i),
+				// Ajoutez plus de propriétés ici jusqu'à Propriete10
+			});
+		}
+	}
+
+	public class MyData
+	{
+		public string Propriete1 { get; set; }
+		public string Propriete0 { get; set; }
+		// Ajoutez plus de propriétés ici jusqu'à Propriete10
+	}
+
+	//public void Button_Jalon1_Clicked(object sender )
+	//{
+		
+	//}
+}
---
+++ AvaloniaApplication_MVVM_1/ViewModels/ViewModelBase.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+public class ViewModelBase : INotifyPropertyChanged
+{
+	public event PropertyChangedEventHandler PropertyChanged;
+
+	public void OnPropertyChanged(string propertyName)
+	{
+		var handler = PropertyChanged;
+		if (handler != null)
+			handler(this, new PropertyChangedEventArgs(propertyName));
+	}
+
+	protected bool SetField<T>(ref T field, T value, [CallerMemberName] string propertyName = null)
+	{
+		if (EqualityComparer<T>.Default.Equals(field, value)) return false;
+		field = value;
+		OnPropertyChanged(propertyName);
+		return true;
+	}
+}
